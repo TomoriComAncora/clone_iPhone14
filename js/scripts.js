d41c80abca861ff3418a0ba35d00fecd19caa486
TomoriComAncora <lucas_fernando@ufms.br>
2023-08-28 20:11:35 -0400

    fim_inspiracao

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-console.log("teste");
+const buttons = document.querySelectorAll("#picker li");
+const image = document.querySelector("#product-image");
+
+buttons.forEach((btns) => {
+  btns.addEventListener("click", (e) => {
+    buttons.forEach((btns) => {
+      btns.querySelector(".color").classList.remove("selected");
+    });
+
+    const button = e.target;
+    const id = button.getAttribute("id");
+
+    button.querySelector(".color").classList.add("selected");
+
+    image.classList.add("changing");
+    image.setAttribute("src", `img/iphone_${id}.jpg`);
+
+    setTimeout(() => {
+      image.classList.remove("changing");
+    }, 200);
+  });
+});
